1a9a9bf25308c0526fa8e276de1aae72eda08491
danil1411 <dania14@ukr.net>
2022-09-07 14:05:38 +0300

    Добавьте файлы проекта.

---
+++ ZT1/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace ZT1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtBin = new System.Windows.Forms.TextBox();
+            this.txtDec = new System.Windows.Forms.TextBox();
+            this.txtBytesBin = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtBytesDec = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(36, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Файл:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(525, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Розмір:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(131, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Область візуалізації:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(36, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Висота:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(174, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ширина:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 512);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label6.Location = new System.Drawing.Point(529, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Файл1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label7.Location = new System.Drawing.Point(529, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Файл2:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label8.Location = new System.Drawing.Point(795, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Байт:";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Enabled = false;
+            this.txtSize.Location = new System.Drawing.Point(580, 45);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.ReadOnly = true;
+            this.txtSize.Size = new System.Drawing.Size(63, 20);
+            this.txtSize.TabIndex = 9;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Enabled = false;
+            this.txtFile.Location = new System.Drawing.Point(88, 45);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(220, 20);
+            this.txtFile.TabIndex = 10;
+            // 
+            // txtBin
+            // 
+            this.txtBin.Enabled = false;
+            this.txtBin.Location = new System.Drawing.Point(580, 99);
+            this.txtBin.Name = "txtBin";
+            this.txtBin.ReadOnly = true;
+            this.txtBin.Size = new System.Drawing.Size(132, 20);
+            this.txtBin.TabIndex = 11;
+            this.txtBin.Text = "FileBinary.txt";
+            // 
+            // txtDec
+            // 
+            this.txtDec.Enabled = false;
+            this.txtDec.Location = new System.Drawing.Point(580, 140);
+            this.txtDec.Name = "txtDec";
+            this.txtDec.ReadOnly = true;
+            this.txtDec.Size = new System.Drawing.Size(132, 20);
+            this.txtDec.TabIndex = 12;
+            this.txtDec.Text = "FileDecimal.txt";
+            // 
+            // txtBytesBin
+            // 
+            this.txtBytesBin.Enabled = false;
+            this.txtBytesBin.Location = new System.Drawing.Point(750, 99);
+            this.txtBytesBin.Name = "txtBytesBin";
+            this.txtBytesBin.ReadOnly = true;
+            this.txtBytesBin.Size = new System.Drawing.Size(132, 20);
+            this.txtBytesBin.TabIndex = 13;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(88, 140);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(54, 20);
+            this.txtY.TabIndex = 14;
+            this.txtY.TextChanged += new System.EventHandler(this.textBoxInputNumbers_TextChanged);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(242, 140);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(66, 20);
+            this.txtX.TabIndex = 15;
+            this.txtX.TextChanged += new System.EventHandler(this.textBoxInputNumbers_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(328, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Обрати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(328, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Малювати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(933, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Записати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtBytesDec
+            // 
+            this.txtBytesDec.Enabled = false;
+            this.txtBytesDec.Location = new System.Drawing.Point(750, 140);
+            this.txtBytesDec.Name = "txtBytesDec";
+            this.txtBytesDec.ReadOnly = true;
+            this.txtBytesDec.Size = new System.Drawing.Size(132, 20);
+            this.txtBytesDec.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 759);
+            this.Controls.Add(this.txtBytesDec);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtBytesBin);
+            this.Controls.Add(this.txtDec);
+            this.Controls.Add(this.txtBin);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Карта файлу";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtBin;
+        private System.Windows.Forms.TextBox txtDec;
+        private System.Windows.Forms.TextBox txtBytesBin;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtBytesDec;
+    }
+}
+
---
+++ ZT1/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ZT1
+{
+    public partial class Form1 : Form
+    {
+        string array;
+        byte[] fileBytes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBoxInputNumbers_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBoxInput = (TextBox)sender;
+            bool checkISNumber = int.TryParse(textBoxInput.Text, out int buffer);
+            if (checkISNumber != true)
+            {
+                if (textBoxInput.Text.Length != 0)
+                {
+                    textBoxInput.Text = textBoxInput.Text.Remove((textBoxInput.Text.Length - 1));
+                    textBoxInput.SelectionStart = textBoxInput.Text.Length;
+                    textBoxInput.SelectionLength = 0;
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+
+            OpenFileDialog opfd = new OpenFileDialog();
+            opfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            opfd.FilterIndex = 2;
+
+
+            if (opfd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = opfd.FileName;
+                var fileSize = new FileInfo(opfd.FileName).Length / 1024;
+                var fileStream = opfd.OpenFile();
+
+               
+                txtFile.Text = filePath;
+                txtSize.Text = fileSize.ToString() + " Kb";
+                if (fileSize < 100000)
+                {
+
+                button2.Enabled= true;
+                button3.Enabled = true;
+               
+                    fileBytes = File.ReadAllBytes(filePath);
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (byte b in fileBytes)
+                    {
+                        sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+
+                    }
+
+                    array = sb.ToString();
+                }
+                else { MessageBox.Show("Файл занадто великий, оберіть файл менший за 100мб"); }
+
+            }
+            else { MessageBox.Show("Оберіть файл у провіднику"); }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            int x, y;
+
+            if (txtY.Text != string.Empty || txtX.Text != string.Empty)
+            {
+
+                int height = Convert.ToInt32(txtY.Text);
+                int width = Convert.ToInt32(txtX.Text);
+                
+                if (height > 0 && height < 513 && width > 0 && width < 1025)
+                {
+                    pictureBox1.Size= new Size(width, height);
+                    try
+                    {
+                        // Retrieve the image.
+                        Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+                        // Loop through the images pixels to reset color.
+                        for (y = 0; y < height; y++)
+                        {
+
+                            for (x = 0; x < width; x++)
+                            {
+
+                                if (k < array.Length)
+                                {
+                                    if (array[k] == '1') { bmp.SetPixel(x, y, Color.Black); }
+                                    if (array[k] == '0') { bmp.SetPixel(x, y, Color.White); }
+                                    k++;
+                                }
+                                else { bmp.SetPixel(x, y, Color.Gray); }
+
+
+
+                            }
+                        }
+
+                        // Set the PictureBox to display the image.
+                        pictureBox1.Image = bmp;
+
+                        // Display the pixel format in Label1.
+
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("There was an error." +
+                            "Try to write another resolution");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Введіть висоту від 0 до 512 та ширину від 0 до 1024");
+                }
+            }
+            else { MessageBox.Show("Введіть висоту від 0 до 512 та ширину від 0 до 1024"); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nameBin = txtBin.Text;
+            string nameDec = txtDec.Text;
+            
+            
+
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(nameBin, false))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in fileBytes)
+                {
+                    
+                    writer.Write(Convert.ToString(b, 2).PadLeft(8, '0')+" ");
+
+                }
+             
+            }
+
+            using (StreamWriter writer = new StreamWriter(nameDec, false))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in fileBytes)
+                {
+
+                    writer.Write(b.ToString() + " ");
+
+                }
+
+
+            }
+
+            string directory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            System.IO.FileInfo fileBin = new System.IO.FileInfo(directory + @"\FileBinary.txt");
+            System.IO.FileInfo fileDec = new System.IO.FileInfo(directory + @"\FileDecimal.txt");
+            long sizeBin = fileBin.Length;
+            long sizeDec = fileDec.Length;
+            txtBytesBin.Text = sizeBin.ToString();
+
+            txtBytesDec.Text = sizeDec.ToString();
+
+
+        }
+    }
+}
